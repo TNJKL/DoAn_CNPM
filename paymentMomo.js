7f7ce58685388f34912f73269033060fbc3b025d
TNJKL <sinhngay21042004@gmail.com>
2025-06-16 14:09:25 +0700

    CNPM-11 #done: Hoan thanh giao dien Momo

--- paymentMomo.js
+++ paymentMomo.js
@@ -2,10 +2,4 @@ const momo = "Payment via MoMo";
 console.log(momo);
 
 
-cancelAnimationFrame = "Cancel Animation Frame";
-console.log(cancelAnimationFrame);
-
-cancelAnimationFrame = "Cancel Animation Frame";
-console.log(cancelAnimationFrame);
-cancelAnimationFrame = "Cancel Animation Frame";
-console.log(cancelAnimationFrame);
+console.log("Side to side brother");
