ca30c54681b2d2fece07b02e99a90ddad75c23ef
TNJKL <sinhngay21042004@gmail.com>
2025-06-16 14:06:01 +0700

    CNPM-191 #done: Tao giao dien trang Momo

--- paymentMomo.js
+++ paymentMomo.js
@@ -4,3 +4,8 @@ console.log(momo);
 
 cancelAnimationFrame = "Cancel Animation Frame";
 console.log(cancelAnimationFrame);
+
+cancelAnimationFrame = "Cancel Animation Frame";
+console.log(cancelAnimationFrame);
+cancelAnimationFrame = "Cancel Animation Frame";
+console.log(cancelAnimationFrame);
